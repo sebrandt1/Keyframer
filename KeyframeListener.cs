0d998006516b1a2c4eca6d58968a0023c8462e7f
sebrandt1 <43499786+sebrandt1@users.noreply.github.com>
2020-09-28 18:36:12 +0200

    Add files via upload

---
+++ KeyframeListener.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class KeyframeListener : MonoBehaviour
+{
+    private AudioSource audioClip;
+    const string summaryOne = "Higher sample rates will be more precise since there will be more samples per band. The calculation will be; Sample / 8 (bands).\n\n";
+    const string summaryTwo = "i.e: 512 samples / 8 bands = 64 samples per band and then we calculate the average of the 64 samples in each band to get our target value.\n\n";
+    const string summaryThree = "1024 samples / 8 bands = 128 samples per band and then we calculate the average of the 128 samples in each band to get our target value.";
+    //Sample sizing must be a power of 2 from min 64 to max 8192 but we want our min to 512 since anything lower will have large inaccuracies.
+    [
+        RangeTargets(512, 1024, 2048, 4096, 8192),
+        Tooltip(summaryOne + summaryTwo + summaryThree)
+    ]
+    public int samples;
+    public static float[] generateSamples;
+    public static float[] frequencyBands = new float[8];
+    public static float[] bandBuffer = new float[8];
+    private float[] bufferDecrease = new float[8];
+
+    void Start()
+    {
+        if (GetComponent<AudioSource>() != null)
+        {
+            //Generate the samples according to the inputted sample sizing from inspector
+            generateSamples = new float[GetSampleSize()];
+            audioClip = GetComponent<AudioSource>();
+        }
+    }
+
+    void Update()
+    {
+        if (audioClip != null)
+        {
+            GetAudioSourceSpectrum();
+            FrequencyBands();
+            BandBuffer();
+        }
+    }
+
+    void GetAudioSourceSpectrum()
+    {
+        audioClip.GetSpectrumData(generateSamples, 0, FFTWindow.Blackman);
+    }
+
+
+    void FrequencyBands()
+    {
+        int count = 0;
+        for (int i = 0; i < frequencyBands.Length; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            if (i == 7)
+            {
+                sampleCount += 2;
+            }
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += generateSamples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+            frequencyBands[i] = average * 10;
+        }
+    }
+
+    void BandBuffer()
+    {
+        for (int i = 0; i < frequencyBands.Length; ++i)
+        {
+            if (frequencyBands[i] > bandBuffer[i])
+            {
+                bandBuffer[i] = frequencyBands[i];
+                bufferDecrease[i] = 0.005f;
+            }
+
+            //if (frequencyBands[i] < bandBuffer[i])
+            //{
+            //    bandBuffer[i] -= bufferDecrease[i];
+            //    bufferDecrease[i] *= 1.4f;
+            //}
+            if (frequencyBands[i] < bandBuffer[i])
+            {
+                bufferDecrease[i] = (bandBuffer[i] - frequencyBands[i]) / 8;
+                bandBuffer[i] -= bufferDecrease[i];
+            }
+        }
+    }
+
+    private int GetSampleSize()
+    {
+        //since our slider will be a value between 0-4 we need to return the correct sample sizing according to slider value
+        //since we DEFINITELY do not want any sample size to be ie 2
+        switch(samples)
+        {
+            case 0: return 512;
+            case 1: return 1024;
+            case 2: return 2048;
+            case 3: return 4096;
+            case 4: return 8192;
+        }
+        //default the value to 512 samples if the slider somehow fucks up (lol)
+        return 512;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+public sealed class RangeTargets : PropertyAttribute
+{
+    public int[] targetValues;
+
+    public RangeTargets(params int[] values)
+    {
+        targetValues = values;
+    }
+}
+
+[CustomPropertyDrawer(typeof(RangeTargets))]
+public sealed class RangeTargetsDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        RangeTargets targets = (RangeTargets)base.attribute;
+        //position the value of our selected sample size next to the slider
+        EditorGUI.LabelField(new Rect(new Vector2(position.x * 6f, position.y), position.size), targets.targetValues[property.intValue].ToString());
+        //create our slider which will be min 0 and max equal to the size of the target values array length - 1
+        EditorGUI.IntSlider(position, property, 0, targets.targetValues.Length - 1);
+    }
+}
+
---
+++ ShaderKeyFramer.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class ShaderKeyFramer : MonoBehaviour
+{
+    [Tooltip("Toggle this if you want to use a single common frequency for all properties.")]
+    public bool globalFrequency;
+    [Tooltip("The single common frequency to use if Use Same Frequency is toggled on."), Range(0, 7)]
+    public int frequency;
+    [Tooltip("Toggle this if you want to set a global value to all filters.")]
+    public bool globalFilter;
+    [Tooltip("Filter any value below this.")]
+    public float globalFilterAllBelow;
+    [Tooltip("All filtered values will be equal to this.")]
+    public float globalFilterTo;
+    public AnimationClip animation;
+    public List<ShaderProperties> properties;
+    private AnimationCurve[] curve;
+    [HideInInspector]
+    public Material material;
+    [HideInInspector]
+    public Shader shader;
+    private List<ShaderProperties> keyframeProperties;
+
+    void Start()
+    {
+        //Clear animation so we get a fresh one with no keyframes or curves
+        animation.ClearCurves();
+        material = GetComponent<Renderer>().material;
+
+        //Create a new list of the ShaderProperties that has Keyframing enabled (So we dont uselessly write to non existent curves)
+        keyframeProperties = properties.Where(x => x._UseForKeyframing).ToList();
+
+        List<AnimationCurve> c = new List<AnimationCurve>();
+        for(int i = 0; i < keyframeProperties.Count; i++)
+        {
+            //add animationcurve for every shaderproperty that we want to keyframe
+            c.Add(new AnimationCurve()); 
+        }
+        curve = c.ToArray();
+    }
+
+    void Update()
+    {
+        //Making sure our material and animation are not null and our curve length is greater than 0
+        //if curve would be 0 and we dont have this check we would keyframe into non existent curves
+        if(material != null && animation != null && curve.Length > 0)
+        {
+            for(int i = 0; i < keyframeProperties.Count; i++)
+            {
+                string targetProperty = keyframeProperties[i]._TrueName;
+
+                if (ShouldKeyFrame(keyframeProperties[i]) && keyframeProperties[i]._PropertyType != ShaderUtil.ShaderPropertyType.Color)
+                {
+                    //Set the animation curve for the current property index
+                    animation.SetCurve("", typeof(MeshRenderer), $"material.{targetProperty}", curve[i]);
+                    //Calculate and set the value of our current target property
+                    material.SetFloat(targetProperty, CalculateShaderSync(keyframeProperties[i]));
+                    //Add the keyframe key to the animation and set it to the current material value (with all audio calculations)
+                    curve[i].AddKey(Time.time, material.GetFloat(targetProperty));
+                }
+                //else if (ShouldKeyFrame(keyframeProperties[i]) && keyframeProperties[i]._PropertyType == ShaderUtil.ShaderPropertyType.Color)
+                //{
+                //    //Color keyframing here
+                //}
+                //if debug logging is enabled we want to print the property name with the property current value 
+                //this will allow us to see what values we wish to manually set to filter out with our filter variables
+                if (keyframeProperties[i]._LogValue)
+                {
+                    Debug.Log($"{keyframeProperties[i]._Name}: {material.GetFloat(targetProperty)} Type: {keyframeProperties[i]._PropertyType}");
+                }
+            }
+        }
+    }
+
+    private float CalculateShaderSync(ShaderProperties sp)
+    {
+        float result = sp._Offset + KeyframeListener.frequencyBands[sp._Frequency] * sp._Strength;
+
+        //if UseFilter is enabled we want to set all values that are under the FilterAllBelow to the selected filter value (FilterTo)
+        //this is so we can filter out minor values so we can ignore keyframing values that are lower than ie 0.3 => 0
+        if (sp._UseFilter)
+        {
+            if (result < sp._FilterAllBelow) result = sp._FilterTo;
+        }
+        return result;
+    }
+
+    private Color CalculateColorSync(ShaderProperties sp)
+    {
+        sp._Color.r = sp._Color.r * (sp._Offset + KeyframeListener.frequencyBands[sp._Frequency] * sp._Strength);
+        sp._Color.b = sp._Color.b * (sp._Offset + KeyframeListener.frequencyBands[sp._Frequency] * sp._Strength);
+        sp._Color.g = sp._Color.g * (sp._Offset + KeyframeListener.frequencyBands[sp._Frequency] * sp._Strength);
+
+        return sp._Color;
+    }
+
+    private bool ShouldKeyFrame(ShaderProperties sp)
+    {
+        bool result = true;
+
+        //if we want to keyframe between a specific time frame (ie from 3 seconds to 6 seconds) 
+        if(sp._SpecificTime)
+        {
+            //return true if current time is more than or equal to start time and less than or equal to end time, else we should not keyframe it
+            result = Time.time >= sp._StartTime && Time.time <= sp._EndTime;
+        }
+        return result;
+    }
+}
+
+//Custom editor so we can set values before playing scene
+[CustomEditor(typeof(ShaderKeyFramer), true)]
+public class ShaderEditor : Editor
+{
+    private ShaderKeyFramer skf = null;
+    private string searchInput = string.Empty;
+
+    public void OnEnable()
+    {
+        if (skf == null) AddKeyFramer(); //Creates the serialized object
+        if (skf.properties == null) AddProperties(); //Adds all the shader properties to the scripts list
+        //Remove collider from object to prevent funky things when uploading later
+        if(skf.gameObject.GetComponent<Collider>() != null)
+        {
+            DestroyImmediate(skf.gameObject.GetComponent<Collider>());
+        }
+
+        skf.properties = skf.properties
+                        .Where(x =>
+                        !x._Name.ToLower().StartsWith("is ") ||
+                        !x._Name.ToLower().Contains("active"))
+                        .OrderBy(x => x._Name)
+                        .ToList();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        //Draw the animation and same frequency options in inspector
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("animation"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("globalFrequency"));
+
+        //If Use Same Frequency is enabled we want to use the same frequency on all shaderproperties
+        if (serializedObject.FindProperty("globalFrequency").boolValue)
+        {
+            //Draw the frequency variable window
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("frequency"));
+            ChangeValue(skf.properties.ToArray(), "frequency");
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("globalFilter"));
+
+        //If global filter is enabled we want to use the same filters on all of our shaderproperties, so we need to set them accordingly
+        if (serializedObject.FindProperty("globalFilter").boolValue)
+        {
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("globalFilterAllBelow"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("globalFilterTo"));
+            ChangeValue(skf.properties.ToArray(), "filter");
+        }
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        //Search bar to make editing easier so you can find a specific property
+        GUILayout.BeginHorizontal(GUI.skin.FindStyle("Toolbar"));
+        searchInput = GUILayout.TextField(searchInput, GUI.skin.FindStyle("ToolbarSeachTextField"));
+        if (GUILayout.Button("", GUI.skin.FindStyle("ToolbarSeachCancelButton")))
+        {
+            searchInput = "";
+            GUI.FocusControl(null);
+        }
+        GUILayout.EndHorizontal();
+
+        //if searchinput is not empty we want to display the elements that contains the search string and filter out the ones that dont
+        if (!string.IsNullOrEmpty(searchInput))
+        {
+            DisplaySingleElement(serializedObject.FindProperty("properties"));
+        }
+        else
+        {
+            DisplayAll(serializedObject.FindProperty("properties"));
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    //Change the targetted variable and set the list to obtain the new values
+    public void ChangeValue(ShaderProperties[] prop, string varName)
+    {
+        ShaderProperties[] array = skf.properties.ToArray();
+        for (int i = 0; i < skf.properties.Count; i++)
+        {
+            switch(varName.ToLower())
+            {
+                case "frequency":
+                    array[i]._Frequency = skf.frequency;
+                    break;
+
+                case "filter":
+                    array[i]._FilterAllBelow = skf.globalFilterAllBelow;
+                    array[i]._FilterTo = skf.globalFilterTo;
+                    array[i]._UseFilter = skf.globalFilter;
+                    break;
+            }
+        }
+        skf.properties = array.ToList();
+    }
+
+    //This will be called when gameobject is selected (through onEnable) and set our ShaderKeyFramer object target
+    public void AddKeyFramer()
+    {
+        skf = (ShaderKeyFramer)target;
+        skf.material = skf.gameObject.GetComponent<Renderer>().sharedMaterial;
+        skf.shader = skf.material.shader;
+    }
+
+    //Dynamically create a shaderproperty for each shader property in the actual shader target
+    public void AddProperties()
+    {
+        List<ShaderProperties> p = new List<ShaderProperties>();
+
+        //Check each of the properties in the actual shader target and create out ShaderProperties structure accordingly
+        for (int i = 0; i < ShaderUtil.GetPropertyCount(skf.shader); i++)
+        {
+            //Check for the type of the shader property index since there are some types we never want to change (ie vectors)
+            ShaderUtil.ShaderPropertyType type = ShaderUtil.GetPropertyType(skf.shader, i);
+
+            //the true variable inside the shader target (ie _ZoomValue)
+            string trueName = ShaderUtil.GetPropertyName(skf.shader, i);
+            string displayName = string.Empty;
+
+            //Create a "clean" string of the shader property name (ie _ZoomValue to Zoom Value) that we'll use as the display name
+            for (int k = 0; k < trueName.Length; k++)
+            {
+                if (char.IsUpper(trueName[k]) && k != 1) displayName += $" {trueName[k]}";
+                else displayName += trueName[k];
+            }
+
+            ShaderProperties prop = new ShaderProperties();
+
+            //since we dont want to modify textures we'll skip all of these
+            if (type != ShaderUtil.ShaderPropertyType.TexEnv)
+            {
+                //Create our properties structure and add it to our list
+                prop = new ShaderProperties
+                {
+                    _TrueName = trueName,
+                    _Name = $"{displayName.Replace("_", "")} ({type.ToString()})",
+                    _Strength = 0,
+                    _Offset = 0,
+                    _UseForKeyframing = false,
+                    _UseFilter = false,
+                    _LogValue = false,
+                    _Frequency = 0,
+                    _PropertyType = type
+                };
+                p.Add(prop);
+            }
+        }
+
+        //Update the ShaderKeyFramers shaderproperties list with the list of structures we created in this method
+        skf.properties = p;
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    public void DisplayAll(SerializedProperty list)
+    {
+        for (int i = 0; i < list.arraySize; i++)
+        {
+            //Use .hasChildren to make sure all of the subelements of an element shows up in the list
+            EditorGUILayout.PropertyField(list.GetArrayElementAtIndex(i), list.GetArrayElementAtIndex(i).hasChildren);
+        }
+    }
+
+    public void DisplaySingleElement(SerializedProperty list)
+    {
+        for (int i = 0; i < list.arraySize; i++)
+        {
+            //Convert displayname to lower and compare it to searchinput to lower so we can search without case sensitivity
+            if(list.GetArrayElementAtIndex(i).displayName.ToLower().Contains(searchInput.ToLower()))
+                EditorGUILayout.PropertyField(list.GetArrayElementAtIndex(i), list.GetArrayElementAtIndex(i).hasChildren);
+        }
+    }
+}
+
+//haha cant use abstraction for inspector haha
+[System.Serializable]
+public struct ShaderProperties
+{
+    [HideInInspector]
+    public string _Name;
+    [HideInInspector]
+    public string _TrueName;
+    [Tooltip("Use this to toggle whether you want to use this property or not. (off by default)")]
+    public bool _UseForKeyframing;
+    [Space(10)]
+    [Tooltip("The strength that the float should be multiplied by in the selected frequency.")]
+    public float _Strength;
+    [Tooltip("Will be additively added in the strength calculation.")]
+    public float _Offset;
+    [Range(0, 7), Tooltip("The audio frequency band to sync with.")]
+    public int _Frequency;
+    [Space(10)]
+    [Tooltip("Toggle if you want to set a custom filter value.")]
+    public bool _UseFilter;
+    [Tooltip("Ignores any value below this to filter out subtle and very minor changes.")]
+    public float _FilterAllBelow;
+    [Tooltip("The value you want to filter to (set it to the default \"resting\" value of the property)")]
+    public float _FilterTo;
+    [Space(10)]
+    [Tooltip("Toggle to show the value in the debug log so you can see what values you want to filter out in smaller changes of audio.")]
+    public bool _LogValue;
+    [Space(10)]
+    [Tooltip("Toggle this if you want to keyframe between specific times (ie from 5 seconds to 15 seconds).")]
+    public bool _SpecificTime;
+    [Tooltip("Start keyframing this property at this time (If Specific Time is toggled).")]
+    public float _StartTime;
+    [Tooltip("Stop keyframing this property at this time (If Specific Time is toggled).")]
+    public float _EndTime;
+    [Tooltip("This will set a default color value for this property that'll fade as the frequency band gets weaker and increase in intesity if the frequency band becomes stronger.")]
+    public Color _Color;
+    [HideInInspector]
+    public ShaderUtil.ShaderPropertyType _PropertyType;
+}
